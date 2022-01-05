3ce163a7f203daa8a84465e11d5886d67dd6aab2
alsruf36 <alsruf36@gmail.com>
2022-01-05 23:33:39 +0900

    added

---
+++ components/LoadMain.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex items-center justify-center h-screen mx-auto bg-purple-100">
+        <div class="items-center p-5 bg-white rounded-lg shadow-xl">
+            <h1 class="text-2xl font-extrabold underline decoration-indigo-500/30">안녕하세요 👋</h1>
+            <h2 class="text-2xl font-extrabold">정치 성향 판별기 입니다.</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'LoadMain',
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <Tutorial />
+  <div>
+    <LoadMain />
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'IndexPage',
-}
+export default {}
 </script>
