f49f8d58391336e579e083286a17dd1303fc5ec1
root <root@u20.04>
2022-04-13 22:11:53 +0900

    add transition

--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,51 @@
 <template>
   <div>
     <img :src="require('~/static/beams.jpg')" alt="" class="absolute top-1/2 left-1/2 max-w-none -translate-x-1/2 -translate-y-1/2 h-full w-full" width="2000" />
-    
-    <transition>
-      <div
-        v-if="CardStatus === 0"
-        class="relative flex items-center justify-center h-screen"
-      >
-          <div class="flex flex-col gap-2">
-              <main-intro />
-              <div class="grid grid-cols-2 grid-flow-col gap-2">
-                <div class="form-control w-full">
-                  <select
-                    class="select select-bordered h-full shadow-xl font-sans font-bold text-2xl"
-                    v-on:change="$i18n.setLocale($event.target.value)">
-                    <option disabled selected>{{ $t('Intro.changeLanguage') }}</option>
-                    <option
-                      v-for="locale in availableLocales"
-                      :key="locale.code"
-                      :value="locale.code">{{ locale.name }}</option>
-                  </select>
-                </div>
-                <button v-on:click="CardStatus = 1" class="items-center p-3 font-sans font-bold text-2xl bg-white divide-y rounded-lg shadow-xl">{{ $t('Intro.start') }}</button>
-              </div>
-          </div>
-      </div>
 
-      <div
-        v-else-if="CardStatus === 1"
-        class="relative flex items-center justify-center h-screen"
-      >
-        <div class="grid gap-6 xl:grid-rows-2 xl:grid-cols-2 w-[90%] h-[90%]">
-          <cards-news-card class="bg-gray-100 xl:col-start-1 xl:row-span-2 xl:row-start-1"></cards-news-card>
-          <cards-input-card class="bg-gray-100 xl:col-start-2 xl:row-span-1 xl:row-start-1"></cards-input-card>
-          <cards-analyze-card class="bg-gray-100 xl:col-start-2 xl:row-span-1 xl:row-start-2"></cards-analyze-card>
+    <transition-group 
+      enter-active-class="duration-500 ease-out"
+      enter-from-class="transform opacity-0"
+      enter-to-class="opacity-100"
+      leave-active-class="duration-500 ease-in"
+      leave-from-class="opacity-100"
+      leave-to-class="transform opacity-0"
+      mode="out-in"
+    >
+      <div v-if="CardStatus === 0" key="intro">
+        <div class="relative flex items-center justify-center h-screen">
+          <div class="flex flex-col gap-2">
+            <main-intro></main-intro>
+            <div class="grid grid-cols-2 grid-flow-col gap-2">
+              <div class="form-control w-full">
+                <select
+                  class="select select-bordered h-full shadow-xl font-sans font-bold text-2xl"
+                  @change="$i18n.setLocale($event.target.value)">
+                  <option disabled selected>{{ $t('Intro.changeLanguage') }}</option>
+                  <option
+                    v-for="locale in availableLocales"
+                    :key="locale.code"
+                    :value="locale.code">{{ locale.name }}</option>
+                </select>
+              </div>
+              <button class="items-center p-3 font-sans font-bold text-2xl bg-white divide-y rounded-lg shadow-xl" @click="CardStatus = 1">{{ $t('Intro.start') }}</button>
+            </div>
+          </div>
         </div>
       </div>
-    </transition>
-    
-      <div v-shortkey.once="['ctrl', 'r']" @shortkey="ClearStatus()"></div>
 
+      <div v-if="CardStatus === 1" key="maincard">
+        <div class="relative flex items-center justify-center h-screen">
+          <div class="grid gap-6 xl:grid-rows-2 xl:grid-cols-2 w-[90%] h-[90%]">
+            <cards-news-card class="bg-gray-100 xl:col-start-1 xl:row-span-2 xl:row-start-1"></cards-news-card>
+            <cards-input-card class="bg-gray-100 xl:col-start-2 xl:row-span-1 xl:row-start-1"></cards-input-card>
+            <cards-analyze-card class="bg-gray-100 xl:col-start-2 xl:row-span-1 xl:row-start-2"></cards-analyze-card>
+            <button @click="ClearStatus()">초기화</button>
+          </div>
+        </div>
+      </div>
+    </transition-group>
+    
+    <div v-shortkey.once="['ctrl', 'r']" @shortkey="ClearStatus()"></div>
   </div>
 </template>
 
@@ -52,13 +58,13 @@ export default {
       isLoading: false
     };
   },
-  created () {
-    this.isLoading = true
-  },
   computed: {
     availableLocales () {
       return this.$i18n.locales.filter(i => i.code !== this.$i18n.locale)
     }
+  },
+  created () {
+    this.isLoading = true
   },
   methods: {
     ClearStatus (event) {
